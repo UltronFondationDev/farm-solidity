9394bf0f08aee4c13505b1611d73aa104cfb5ba7
root <root@code-server3.ru-central1.internal>
2022-05-20 08:01:27 +0000

    wulx interface fix

--- contracts/IwULX.sol
+++ contracts/IwULX.sol
@@ -5,4 +5,5 @@ interface IwULX {
     function mint(address account, uint256 amount) external;
     function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 amount) external returns (bool);
+    function burn(address account, uint256 amount) external;
 }
