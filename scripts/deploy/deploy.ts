5abd9c212cb9c40a060a56cc250a02d485eba8f4
backendultron <backend@ultron.foundation>
2022-06-21 17:42:57 +0300

    add deploy script (Bogdan)

---
+++ scripts/deploy/deploy.ts
@@ -0,0 +1,16 @@
+import { subtask, task, types } from "hardhat/config";
+
+task("deploy", "Deploy MasterChef")
+  .addParam("wulx", "wULX address")
+  .addParam("devaddr", "Dev address")
+  .addParam("persec", "wULX PerSecond")
+  .addParam("starttime", "Start Time", 0, types.int)
+  .setAction(async (taskArgs, {ethers}) => {
+      const MasterChef = await ethers.getContractFactory("MasterChef");
+      const masterChef = await MasterChef.deploy(taskArgs.wulx, taskArgs.devaddr, taskArgs.persec, taskArgs.starttime);
+
+      await masterChef.deployed();
+      MasterChefAddress = masterChef.address
+      console.log("MasterChef deployed to:", masterChef.address);
+      console.log(MasterChefAddress)
+  });
