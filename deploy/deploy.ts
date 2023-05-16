e75511f96ea70ab4c938b6e4fd17714152d8d38d
ultron <software@ultron.foundation>
2023-05-16 15:48:59 +0300

    feat(Sergej): added pool

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -22,12 +22,13 @@ task('set-pool', "Changing pool's alloc point")
         const masterChefAddress = '0x51D62Ca04e955AbB7f8BB380FD6f93E1F7d5E5fC'; // 0x9F8eFbc1A35f9D5941efEA8F8aD30703e667F009
         const masterChef = await ethers.getContractAt("MasterChef", masterChefAddress, signer);
 
-        // await masterChef.set(0,  1069, { gasLimit: 2000000 });
-        // await masterChef.set(1,  594, { gasLimit: 2000000 });
-        // await masterChef.set(2,  119, { gasLimit: 2000000 });
-        // await masterChef.set(3,  356, { gasLimit: 2000000 });
-        // await masterChef.set(4,  237, { gasLimit: 2000000 });
-        // await masterChef.set(5,  2625, { gasLimit: 2000000 });
+        // await masterChef.set(0,  166, { gasLimit: 2000000 });
+        // await masterChef.set(1,  83, { gasLimit: 2000000 });
+        // await masterChef.set(2,  2, { gasLimit: 2000000 });
+        // await masterChef.set(3,  37, { gasLimit: 2000000 });
+        // await masterChef.set(4,  154, { gasLimit: 2000000 });
+        // await masterChef.set(5,  2040, { gasLimit: 2000000 });
+        // await masterChef.set(8,  18, { gasLimit: 2000000 });
 
         // await Helpers.delay(4000);
 
