89c43ecec564a39e9fc2383bbe5df1c9ddbb1168
ultron <software@ultron.foundation>
2023-06-04 12:33:29 +0300

    feat(Sergej): pools

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -22,20 +22,20 @@ task('set-pool', "Changing pool's alloc point")
         const masterChefAddress = '0x51D62Ca04e955AbB7f8BB380FD6f93E1F7d5E5fC'; // 0x9F8eFbc1A35f9D5941efEA8F8aD30703e667F009
         const masterChef = await ethers.getContractAt("MasterChef", masterChefAddress, signer);
 
-        // await masterChef.set(0,  147, { gasLimit: 2000000 });
-        // await masterChef.set(1,  74, { gasLimit: 2000000 });
+        // await masterChef.set(0,  139, { gasLimit: 2000000 });
+        // await masterChef.set(1,  69, { gasLimit: 2000000 });
         // await masterChef.set(2,  2, { gasLimit: 2000000 });
-        // await masterChef.set(3,  37, { gasLimit: 2000000 });
-        // await masterChef.set(4,  150, { gasLimit: 2000000 });
-        // await masterChef.set(5,  2040, { gasLimit: 2000000 });
-        // await masterChef.set(8,  14, { gasLimit: 2000000 });
-        // await masterChef.set(9,  14, { gasLimit: 2000000 });
-        // await masterChef.set(10,  0, { gasLimit: 2000000 });
+        // await masterChef.set(3,  35, { gasLimit: 2000000 });
+        // await masterChef.set(4,  132, { gasLimit: 2000000 });
+        // await masterChef.set(5,  2067, { gasLimit: 2000000 });
+        // await masterChef.set(8,  13, { gasLimit: 2000000 });
+        // await masterChef.set(9,  13, { gasLimit: 2000000 });
+        // await masterChef.set(10,  9, { gasLimit: 2000000 });
         // await masterChef.set(11,  9, { gasLimit: 2000000 });
         // await masterChef.set(12,  0, { gasLimit: 2000000 });
         // await masterChef.set(13,  0, { gasLimit: 2000000 });
         // await masterChef.set(14,  0, { gasLimit: 2000000 });
-        // await masterChef.set(15,  14, { gasLimit: 2000000 });
+        // await masterChef.set(15,  13, { gasLimit: 2000000 });
 
         // await Helpers.delay(4000);
 
